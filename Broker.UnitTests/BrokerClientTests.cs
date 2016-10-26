9b6af8f6226d4b3bbdccc35da5aa60a89185692a
Jeff Purcell <japurcell@users.noreply.github.com>
2016-10-26 10:32:42 -0700

    Implemented all needed functions

--- Broker.UnitTests/BrokerClientTests.cs
+++ Broker.UnitTests/BrokerClientTests.cs
@@ -7,11 +7,172 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Moq;
 using Psns.Common.Clients.Broker;
+using LanguageExt;
+using static LanguageExt.Prelude;
 using static Psns.Common.Clients.Broker.BrokerClient;
 using static LanguageExt.List;
 
 namespace Broker.UnitTests
 {
+    [TestFixture]
+    public class OpenTests : AssertionHelper
+    {
+        [Test]
+        public void OpenOk_ReturnsConnection()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+
+            var connection = Open(() => mockConnection.Object);
+
+            Expect(connection, Is.EqualTo(mockConnection.Object));
+        }
+
+        [Test]
+        public void OpenThrows_ReturnsError()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(c => c.Open()).Throws(new Exception("error"));
+
+            var connection = Open(() => mockConnection.Object);
+
+            Expect(connection.Match(con => string.Empty, error => error), Does.Contain("error"));
+        }
+    }
+
+    [TestFixture]
+    public class BeginTransationTests : AssertionHelper
+    {
+        [Test]
+        public void ConnectionBeginTransactionOk_ReturnsTransaction()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(c => c.BeginTransaction()).Returns(new Mock<IDbTransaction>().Object);
+
+            var transaction = BeginTransaction(Right<string, IDbConnection>(mockConnection.Object));
+
+            Expect(transaction.IsRight, Is.True);
+        }
+
+        [Test]
+        public void ConnectionBeginTransactionThrows_ReturnsError()
+        {
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(c => c.BeginTransaction()).Throws(new Exception("error"));
+
+            var transaction = BeginTransaction(Right<string, IDbConnection>(mockConnection.Object));
+
+            Expect(transaction.Match(trans => string.Empty, error => error), Does.Contain("error"));
+        }
+
+        [Test]
+        public void ConnectionIsLeft_ReturnsError()
+        {
+            var transaction = BeginTransaction(Left<string, IDbConnection>("error"));
+
+            Expect(transaction.Match(trans => string.Empty, error => error), Does.Contain("error"));
+        }
+    }
+
+    [TestFixture]
+    public class CreateCommandTests : AssertionHelper
+    {
+        [Test]
+        public void ConnectionBeginTransactionFactoryIsRight_ReturnsCommand()
+        {
+            var mockCommand = new Mock<IDbCommand>();
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            var mockTransaction = new Mock<IDbTransaction>();
+
+            var command = CreateCommand(() => mockConnection.Object, Right<string, IDbTransaction>(mockTransaction.Object));
+
+            Expect(command, EqualTo(mockCommand.Object));
+            mockCommand.VerifySet(c => c.Transaction = mockTransaction.Object, Times.Once());
+        }
+
+        [Test]
+        public void ConnectionBeginTransactionFactoryIsLeft_ReturnsError()
+        {
+            var mockCommand = new Mock<IDbCommand>();
+            var mockConnection = new Mock<IDbConnection>();
+            mockConnection.Setup(c => c.CreateCommand()).Returns(mockCommand.Object);
+            var mockTransaction = new Mock<IDbTransaction>();
+
+            var command = CreateCommand(() => mockConnection.Object, Left<string, IDbTransaction>("error"));
+
+            Expect(match(command, cmd => "cmd", error => error), EqualTo("error"));
+            mockCommand.VerifySet(c => c.Transaction = mockTransaction.Object, Times.Never());
+        }
+    }
+
+    [TestFixture]
+    public class SendAsyncTests : AssertionHelper
+    {
+        Mock<IDbCommand> _mockCommand;
+        Mock<IDataParameterCollection> _mockParams;
+        List<SqlParameter> _addedParams;
+
+        [SetUp]
+        public void Setup()
+        {
+            _addedParams = new List<SqlParameter>();
+            _mockParams = new Mock<IDataParameterCollection>();
+            _mockCommand = new Mock<IDbCommand>();
+            _mockCommand.Setup(c => c.Parameters).Returns(_mockParams.Object);
+        }
+
+        [Test]
+        public async Task SendAsync_CommandIsRight_ReturnsError()
+        {
+            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
+            {
+                var param = obj as SqlParameter;
+                if(param.SqlDbType == SqlDbType.NVarChar)
+                    param.Value = "string";
+                else if(param.SqlDbType == SqlDbType.UniqueIdentifier)
+                    param.Value = Guid.Empty;
+
+                _addedParams.Add(param);
+            });
+
+            var unit = await SendAsync(
+                () => Task.FromResult(1),
+                new BrokerMessage("type", string.Empty, Guid.Empty, Guid.Empty),
+                Right<string, IDbCommand>(_mockCommand.Object));
+
+            Expect(unit, EqualTo(Unit.Default));
+
+            _mockCommand.VerifySet(c => c.CommandText = "SEND ON CONVERSATION @conversation MESSAGE TYPE [type] (@message)", Times.Once());
+
+            iter(new[] { "@message", "@conversation" }, name =>
+            {
+                Expect(exists(_addedParams, param => param.ParameterName == name), Is.True);
+            });
+        }
+
+        [Test]
+        public async Task SendAsync_CommandIsLeft_ReturnsUnit()
+        {
+            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
+            {
+                var param = obj as SqlParameter;
+                if(param.SqlDbType == SqlDbType.NVarChar)
+                    param.Value = "string";
+                else if(param.SqlDbType == SqlDbType.UniqueIdentifier)
+                    param.Value = Guid.Empty;
+
+                _addedParams.Add(param);
+            });
+
+            var result = await SendAsync(
+                () => Task.FromResult(1),
+                new BrokerMessage("type", string.Empty, Guid.Empty, Guid.Empty),
+                Left<string, IDbCommand>("error"));
+
+            Expect(match(from r in result select r, unit => string.Empty, error => error), Does.Contain("error"));
+        }
+    }
+
     [TestFixture]
     public class ReceiveAsyncTests : AssertionHelper
     {
@@ -28,10 +189,27 @@ namespace Broker.UnitTests
             _mockCommand.Setup(c => c.Parameters).Returns(_mockParams.Object);
         }
 
-        [TearDown]
-        public void Teardown()
+        [Test]
+        public async Task ReceiveAsync_ShouldExecuteQueryAndReturnNonEmptyMessageWhenMessageHasValue()
         {
-            Expect(_addedParams, Has.Count.EqualTo(4));
+            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
+            {
+                var param = obj as SqlParameter;
+                if(param.SqlDbType == SqlDbType.NVarChar)
+                    param.Value = "string";
+                else if(param.SqlDbType == SqlDbType.UniqueIdentifier)
+                    param.Value = Guid.Empty;
+
+                _addedParams.Add(param);
+            });
+
+            var message = await ReceiveAsync(
+                token => Task.FromResult(1),
+                "queue",
+                new CancellationTokenSource().Token,
+                Right<string, IDbCommand>(_mockCommand.Object));
+
+            Expect(message, EqualTo(new BrokerMessage("string", "string", Guid.Empty, Guid.Empty)));
 
             _mockCommand.VerifySet(c => c.CommandText = "WAITFOR (RECEIVE TOP(1) " +
                 "@messageType = message_type_name, " +
@@ -47,25 +225,6 @@ namespace Broker.UnitTests
         }
 
         [Test]
-        public async Task ReceiveAsync_ShouldExecuteQueryAndReturnNonEmptyMessageWhenMessageHasValue()
-        {
-            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
-            {
-                var param = obj as SqlParameter;
-                if(param.SqlDbType == SqlDbType.NVarChar)
-                    param.Value = "string";
-                else if(param.SqlDbType == SqlDbType.UniqueIdentifier)
-                    param.Value = Guid.Empty;
-
-                _addedParams.Add(param);
-            });
-
-            var message = await ReceiveAsync("queue", new CancellationTokenSource().Token, () => _mockCommand.Object, token => Task.FromResult(1));
-
-            Expect(message, EqualTo(new BrokerMessage("string", "string", Guid.Empty, Guid.Empty)));
-        }
-
-        [Test]
         public async Task ReceiveAsync_ShouldExecuteQueryAndReturnEmptyMessageWhenMessageIsNull()
         {
             _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
@@ -76,9 +235,129 @@ namespace Broker.UnitTests
                 _addedParams.Add(param);
             });
 
-            var message = await ReceiveAsync("queue", new CancellationTokenSource().Token, () => _mockCommand.Object, token => Task.FromResult(1));
+            var message = await ReceiveAsync(
+                token => Task.FromResult(1),
+                "queue",
+                new CancellationTokenSource().Token,
+                Right<string, IDbCommand>(_mockCommand.Object));
 
             Expect(message, EqualTo(BrokerMessage.Empty));
+
+            _mockCommand.VerifySet(c => c.CommandText = "WAITFOR (RECEIVE TOP(1) " +
+                "@messageType = message_type_name, " +
+                "@message = message_body, " +
+                "@conversationGroup = conversation_group_id, " +
+                "@conversation = conversation_handle " +
+                "FROM [queue]), TIMEOUT 5000;", Times.Once());
+
+            iter(new[] { "@messageType", "@message", "@conversationGroup", "@conversation" }, name =>
+            {
+                Expect(exists(_addedParams, param => param.ParameterName == name), Is.True);
+            });
+        }
+
+        [Test]
+        public async Task ReceiveAsync_CommandIsLeft_ReturnsError()
+        {
+            var message = await ReceiveAsync(
+                token => Task.FromResult(1),
+                "queue",
+                new CancellationTokenSource().Token,
+                Left<string, IDbCommand>("error"));
+
+            Expect(match(message, msg => string.Empty, error => error), Does.Contain("error"));
+        }
+    }
+
+    [TestFixture]
+    public class EndDialogAsyncTests : AssertionHelper
+    {
+        Mock<IDbCommand> _mockCommand;
+        Mock<IDataParameterCollection> _mockParams;
+        List<SqlParameter> _addedParams;
+
+        [SetUp]
+        public void Setup()
+        {
+            _addedParams = new List<SqlParameter>();
+            _mockParams = new Mock<IDataParameterCollection>();
+            _mockCommand = new Mock<IDbCommand>();
+            _mockCommand.Setup(c => c.Parameters).Returns(_mockParams.Object);
+        }
+
+        [Test]
+        public async Task CommandIsRight_ReturnsUnit()
+        {
+            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
+            {
+                var param = obj as SqlParameter;
+                if(param.SqlDbType == SqlDbType.UniqueIdentifier)
+                    param.Value = Guid.Empty;
+
+                _addedParams.Add(param);
+            });
+
+            var unit = await EndDialogAsync(
+                () => Task.FromResult(1),
+                Guid.Empty,
+                Right<string, IDbCommand>(_mockCommand.Object));
+
+            Expect(unit, EqualTo(Unit.Default));
+
+            _mockCommand.VerifySet(c => c.CommandText = "END CONVERSATION @conversation", Times.Once());
+
+            Expect(exists(_addedParams, param => param.ParameterName == "@conversation"), Is.True);
+        }
+
+        [Test]
+        public async Task CommandIsLeft_ReturnsUnit()
+        {
+            _mockParams.Setup(p => p.Add(It.IsAny<object>())).Callback((object obj) =>
+            {
+                var param = obj as SqlParameter;
+                if(param.SqlDbType == SqlDbType.UniqueIdentifier)
+                    param.Value = Guid.Empty;
+
+                _addedParams.Add(param);
+            });
+
+            var result = await EndDialogAsync(
+                () => Task.FromResult(1),
+                Guid.Empty,
+                Left<string, IDbCommand>("error"));
+
+            Expect(match(result, msg => string.Empty, error => error), Does.Contain("error"));
+        }
+    }
+
+    [TestFixture]
+    public class CommitTests : AssertionHelper
+    {
+        [Test]
+        public void CommitOk_ReturnsUnit()
+        {
+            var unit = Commit(Right<string, IDbTransaction>(new Mock<IDbTransaction>().Object));
+
+            Expect(unit, EqualTo(Unit.Default));
+        }
+
+        [Test]
+        public void CommitThrows_ReturnsError()
+        {
+            var mockTransaction = new Mock<IDbTransaction>();
+            mockTransaction.Setup(t => t.Commit()).Throws(new Exception("error"));
+
+            var unit = Commit(Right<string, IDbTransaction>(mockTransaction.Object));
+
+            Expect(match(unit, u => string.Empty, e => e), Does.Contain("error"));
+        }
+
+        [Test]
+        public void TransactionIsLeft_ReturnsError()
+        {
+            var unit = Commit(Left<string, IDbTransaction>("error"));
+
+            Expect(unit.Match(u => string.Empty, error => error), Does.Contain("error"));
         }
     }
 }
--- Psns.Common.Clients.Broker/BrokerClient.cs
+++ Psns.Common.Clients.Broker/BrokerClient.cs
@@ -12,35 +12,82 @@ namespace Psns.Common.Clients.Broker
 {
     public static class BrokerClient
     {
-        public static Either<string, IDbTransaction> BeginTransaction(Func<IDbConnection> connectionFactory)
+        public static Either<string, IDbConnection> Open(Func<IDbConnection> connectionFactory)
         {
+            var connection = connectionFactory();
+
             return match(
-                Try(connectionFactory().BeginTransaction),
-                transaction => Right<string, IDbTransaction>(transaction),
-                exception => Left<string, IDbTransaction>(exception.GetExceptionChainMessages()));
+                Try(() =>
+                {
+                    connection.Open();
+                    return Unit.Default;
+                }),
+                Succ: unit => Right<string, IDbConnection>(connection),
+                Fail: exception => Left<string, IDbConnection>(exception.GetExceptionChainMessages()));
         }
 
-        public static Either<string, IDbCommand> CreateCommand(Func<IDbConnection> connectionFactory, Func<Either<string, IDbTransaction>> transactionFactory)
+        public static Either<string, IDbTransaction> BeginTransaction(Either<string, IDbConnection> connection)
+        {
+            return match(
+                connection,
+                conn => match(
+                        Try(conn.BeginTransaction),
+                        transaction => Right<string, IDbTransaction>(transaction),
+                        exception => Left<string, IDbTransaction>(exception.GetExceptionChainMessages())),
+                error => true == false 
+                    ? Right<string, IDbTransaction>(null) 
+                    : Left<string, IDbTransaction>(error));
+        }
+
+        public static Either<string, IDbCommand> CreateCommand(Func<IDbConnection> connectionFactory, 
+            Either<string, IDbTransaction> transaction)
         {
             var command = connectionFactory().CreateCommand();
 
-            return match(transactionFactory(),
-                transaction =>
+            return match(transaction,
+                Right: trans =>
                 {
-                    command.Transaction = transaction;
+                    command.Transaction = trans;
                     return Right<string, IDbCommand>(command);
                 },
-                error => Left<string, IDbCommand>(error));
+                Left: error => Left<string, IDbCommand>(error));
         }
 
-        public static async Task<Either<string, BrokerMessage>> ReceiveAsync(string queueName, 
-            CancellationToken token, 
-            Func<Either<string, IDbCommand>> commandFactory, 
-            Func<CancellationToken, Task<int>> executeQuery)
+        public static async Task<Either<string, Unit>> SendAsync(
+            Func<Task<int>> executeQuery,
+            BrokerMessage message,
+            Either<string, IDbCommand> command)
         {
             return await match(
-                commandFactory(),
-                async command =>
+                command,
+                Right: async cmd =>
+                {
+                    var parameters = new[]
+                    {
+                        new SqlParameter("@message", SqlDbType.NVarChar, message.Message.Length),
+                        new SqlParameter("@conversation", SqlDbType.UniqueIdentifier)
+                    };
+
+                    iter(parameters, parameter => cmd.Parameters.Add(parameter));
+
+                    cmd.CommandText = "SEND ON CONVERSATION @conversation MESSAGE TYPE [" + message.MessageType + "] (@message)";
+
+                    await executeQuery();
+
+                    return Right<string, Unit>(Unit.Default);
+                },
+                Left: error => Task.FromResult(Left<string, Unit>(error)));
+        }
+
+        public static async Task<Either<string, BrokerMessage>> ReceiveAsync(
+            Func<CancellationToken, Task<int>> executeQuery,
+            string queueName, 
+            CancellationToken token, 
+            Either<string, IDbCommand> command)
+        {
+            return await match(
+                command,
+                Right: async cmd =>
                 {
                     var parameters = new[]
                     {
@@ -53,10 +100,10 @@ namespace Psns.Common.Clients.Broker
                     iter(parameters, parameter =>
                     {
                         parameter.Direction = ParameterDirection.Output;
-                        command.Parameters.Add(parameter);
+                        cmd.Parameters.Add(parameter);
                     });
 
-                    command.CommandText = "WAITFOR (RECEIVE TOP(1) " +
+                    cmd.CommandText = "WAITFOR (RECEIVE TOP(1) " +
                         "@messageType = message_type_name, " +
                         "@message = message_body, " +
                         "@conversationGroup = conversation_group_id, " +
@@ -76,7 +123,48 @@ namespace Psns.Common.Clients.Broker
                     else
                         return Right<string, BrokerMessage>(BrokerMessage.Empty);
                 },
-                error => Task.FromResult(Left<string, BrokerMessage>(error)));
+                Left: error => Task.FromResult(Left<string, BrokerMessage>(error)));
+        }
+
+        public static async Task<Either<string, Unit>> EndDialogAsync(
+            Func<Task<int>> executeQuery,
+            Guid conversation,
+            Either<string, IDbCommand> command)
+        {
+            return await match(
+                command,
+                Right: async cmd =>
+                {
+                    var parameters = new[]
+                    {
+                        new SqlParameter("@conversation", SqlDbType.UniqueIdentifier)
+                    };
+
+                    iter(parameters, parameter => cmd.Parameters.Add(parameter));
+
+                    cmd.CommandText = "END CONVERSATION @conversation";
+
+                    await executeQuery();
+
+                    return Right<string, Unit>(Unit.Default);
+                },
+                Left: error => Task.FromResult(Left<string, Unit>(error)));
+        }
+
+        public static Either<string, Unit> Commit(Either<string, IDbTransaction> transaction)
+        {
+            return match(
+                transaction,
+                Right: trans =>
+                {
+                    return match(
+                        Try(() => { trans.Commit(); return Unit.Default; }),
+                        Succ: unit => Right<string, Unit>(unit),
+                        Fail: exception => Left<string, Unit>(exception.GetExceptionChainMessages()));
+                },
+                Left: error => true == false
+                    ? Right<string, Unit>(Unit.Default)
+                    : Left<string, Unit>(error));
         }
     }
 }
