bc2a0bfca4afe0041caf886524c1e51d4f634fb8
Jeff Purcell <japurcell@users.noreply.github.com>
2017-06-16 07:46:03 -0700

    Handle null values when logging Command; move helper funcs to another file

--- Psns.Common.Clients.Broker/AppPrelude.cs
+++ Psns.Common.Clients.Broker/AppPrelude.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 using LanguageExt;
 using static LanguageExt.List;
@@ -9,7 +8,7 @@ using static LanguageExt.Prelude;
 
 namespace Psns.Common.Clients.Broker
 {
-    public static class AppPrelude
+    public static partial class AppPrelude
     {
         public static readonly Func<IDbConnection, Either<Exception, OpenConnection>> openConnection = connection =>
             safe(() =>
@@ -216,79 +215,11 @@ namespace Psns.Common.Clients.Broker
                     left: error => error);
             };
 
-        [Pure]
-        public async static Task<R2> matchAsync<T, L, R2>(Either<L, T> self, Func<T, Task<R2>> right, Func<L, R2> left) =>
-            await self.MatchAsync(right, left);
-
-        [Pure]
-        public async static Task<R2> matchAsync<T, L, R2>(Task<Either<L, T>> self, Func<T, R2> right, Func<L, R2> left)
-        {
-            var either = await self;
-
-            return match(
-                either,
-                Right: val => right(val),
-                Left: err => left(err));
-        }
-
-        /// <summary>
-        /// Runs a function within a try/catch block
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="fun"></param>
-        /// <returns>A string representation of the exception on failure</returns>
-        [Pure]
-        public static Either<Exception, T> safe<T>(Func<T> fun)
-        {
-            try
-            {
-                return fun();
-            }
-            catch(Exception e)
-            {
-                return e;
-            }
-        }
-
-        [Pure]
-        public async static Task<Either<Exception, T>> safeAsync<T>(Func<Task<T>> f)
-        {
-            try
-            {
-                return await f();
-            }
-            catch(Exception e)
-            {
-                return await Task.FromResult(e);
-            }
-        }
-
-        [Pure]
-        public static Func<T, Option<R>> always<T, R>(T value, Action<T> map) =>
-            (T input) => { map(input); return None; };
-
-        [Pure]
-        public static Either<L, R> Join<L, R>(this Either<L, R> self, Either<L, R> other) =>
-            self.Join(other, o => o, i => i, (o, i) => i);
-
-        [Pure]
-        public static Either<L, R> Join<L, R, U>(this Either<L, R> self, Either<U, R> other, Func<U, L> project) =>
-            self.Join(
-                match(
-                    other,
-                    Right: r => Right<L, R>(r),
-                    Left: u => project(u)));
-
-        [Pure]
-        public static Unit dispose(IDisposable d)
-        {
-            d.Dispose();
-            return Unit.Default;
-        }
-
         static Unit logCommand(Some<string> callerName, IDbCommand command, Action<string> log)
         {
-            log($"{callerName} -> Param Count: {command.Parameters.Count} Connection State: {command.Connection.State.ToString()} Transaction Isolation Leve: {command.Transaction.IsolationLevel.ToString()}");
+            log($@"{callerName} -> Param Count: {command?.Parameters?.Count.ToString() ?? "Null"} Connection State: {
+                command?.Connection?.State.ToString() ?? "Null"} Transaction Isolation Leve: {
+                command?.Transaction?.IsolationLevel.ToString() ?? "Null"}");
             return Unit.Default;
         }
     }
---
+++ Psns.Common.Clients.Broker/Func.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace Psns.Common.Clients.Broker
+{
+    public static partial class AppPrelude
+    {
+        [Pure]
+        public async static Task<R2> matchAsync<T, L, R2>(Either<L, T> self, Func<T, Task<R2>> right, Func<L, R2> left) =>
+            await self.MatchAsync(right, left);
+
+        [Pure]
+        public async static Task<R2> matchAsync<T, L, R2>(Task<Either<L, T>> self, Func<T, R2> right, Func<L, R2> left)
+        {
+            var either = await self;
+
+            return match(
+                either,
+                Right: val => right(val),
+                Left: err => left(err));
+        }
+
+        /// <summary>
+        /// Runs a function within a try/catch block
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fun"></param>
+        /// <returns>A string representation of the exception on failure</returns>
+        [Pure]
+        public static Either<Exception, T> safe<T>(Func<T> fun)
+        {
+            try
+            {
+                return fun();
+            }
+            catch(Exception e)
+            {
+                return e;
+            }
+        }
+
+        [Pure]
+        public async static Task<Either<Exception, T>> safeAsync<T>(Func<Task<T>> f)
+        {
+            try
+            {
+                return await f();
+            }
+            catch(Exception e)
+            {
+                return await Task.FromResult(e);
+            }
+        }
+
+        [Pure]
+        public static Func<T, Option<R>> always<T, R>(T value, Action<T> map) =>
+            (T input) => { map(input); return None; };
+
+        [Pure]
+        public static Either<L, R> Join<L, R>(this Either<L, R> self, Either<L, R> other) =>
+            self.Join(other, o => o, i => i, (o, i) => i);
+
+        [Pure]
+        public static Either<L, R> Join<L, R, U>(this Either<L, R> self, Either<U, R> other, Func<U, L> project) =>
+            self.Join(
+                match(
+                    other,
+                    Right: r => Right<L, R>(r),
+                    Left: u => project(u)));
+
+        [Pure]
+        public static Unit dispose(IDisposable d)
+        {
+            d.Dispose();
+            return Unit.Default;
+        }
+    }
+}
